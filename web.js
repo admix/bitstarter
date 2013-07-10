46399a2b169be6b4df3030b074d4de82c953e05d
admix <admix.software@gmail.com>
2013-07-10 04:06:08 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
-buf = new Buffer(256);
+
 var app = express.createServer(express.logger());
-var fs = require('fs');
-buf[0] = fs.readFileSync('/home/ubuntu/bitstarter/index.html');
-var ret = buf.toString('utf8'); 
+var fs = require("fs");
+var data = fs.readFileSync("/home/ubuntu/bitstarter/index.html");
+ 
 app.get('/', function(request, response) {
-  response.send(ret);
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
